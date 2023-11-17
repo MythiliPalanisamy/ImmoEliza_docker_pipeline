d9c5858f3f0b5250692bf1e4e1368838c9f2e85f
MythiliPalanisamy <102500645+MythiliPalanisamy@users.noreply.github.com>
2023-11-17 06:49:32 +0100

    update

--- docker-airflow/dags/main.py
+++ docker-airflow/dags/main.py
@@ -6,16 +6,12 @@ import scrape_url as scrape_url
 import scrape_data as scrape_data
 import clean_data as cleaning
 import models as models
-import stream as stream
 
 default_args = {
     'owner' : 'airflow',
     'retries': 0, 
     'retry_delay': timedelta(minutes=2)
 }
-
-default_args_for_prediction = {'owner' : 'admin',
-                               'retries' : 0}
 
 with DAG('cleaning_and_training_model', 
             default_args=default_args,
@@ -30,14 +26,6 @@ with DAG('cleaning_and_training_model',
     
     scraping_url >> scraping_data >> clean_data >> training_model 
 
-# streamlit
-with DAG('run_streamlit_app',
-         schedule_interval=None,
-         start_date=datetime(2023, 11, 12),
-         catchup=False) as dag_streamlit:
-
-    streamlit_command = f"streamlit run /opt/airflow/dags/stream.py"
-    run_streamlit_app = BashOperator(task_id='run_streamlit', bash_command=streamlit_command, dag=dag_streamlit)
     
 
 
--- docker-airflow/stream-lit/stream.py
+++ docker-airflow/stream-lit/stream.py
@@ -1,23 +1,26 @@
 import streamlit as st
+import pickle
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
-import aws_s3 as s3
+from dags import aws_s3 as s3
+
 
 
 data = s3.read_data_from_csv('cleaned.csv')
 
 # Streamlit app title
-st.title("Real Estate Property Explorer")
+st.title("Real Estate Property  Predictor and Explorer")
 
 # Add a sidebar for user interaction
 st.sidebar.subheader("Agenda")
 
 # Create a radio button to select agenda items
 selected_agenda_item = st.sidebar.radio("Select one to view", ["Introduction", "Exploring the dataset",
-                                                                "Visualisation", "Interactive Visualisation",
-                                                                "Final notes"])
+                                                                "Visualisation", "Interactive Visualisation", 
+                                                                "Price Prediction", "Final notes"])
 
 # Based on the selected agenda item, display the corresponding content
 if selected_agenda_item == "Introduction":
@@ -40,7 +43,7 @@ elif selected_agenda_item == "Visualisation":
     boxplot_ax.set_ylabel("Price")
     boxplot_ax.set_title("Price Distribution for Houses and Apartments")
     st.pyplot(boxplot_fig)
-
+    """
     # Visualization 1: Histogram of Prices
     st.subheader("Price Distribution")
     fig, ax = plt.subplots(figsize=(10, 6))
@@ -55,7 +58,7 @@ elif selected_agenda_item == "Visualisation":
     sns.scatterplot(data=data, x='bedrooms', y='price', ax=ax)
     plt.xlabel("Number of Bedrooms")
     plt.ylabel("Price")
-    st.pyplot(fig)
+    st.pyplot(fig)"""
 
 elif selected_agenda_item == "Interactive Visualisation":
     # interactive line chart using Plotly to visualize trends over time, such as price changes.
@@ -63,7 +66,7 @@ elif selected_agenda_item == "Interactive Visualisation":
     fig = px.line(data, x="bedrooms", y="price", color="type_of_property", title="Price Trends")
     st.plotly_chart(fig)    
 
-    # interactive scatter plots, which can be useful for exploring relationships between variables.
+    """  # interactive scatter plots, which can be useful for exploring relationships between variables.
     st.subheader("Interactive Scatter Plot")
     fig = px.scatter(data, x="bedrooms", y="price", color="type_of_property", title="Bedrooms vs. Price")
     st.plotly_chart(fig)
@@ -75,7 +78,7 @@ elif selected_agenda_item == "Interactive Visualisation":
     # Interactive bar chart
     st.subheader("Interactive Bar Chart")
     fig = px.bar(data, x="type_of_property", y="price", color="type_of_property", title="Average Price by Property Type")
-    st.plotly_chart(fig)
+    st.plotly_chart(fig)"""
 
 elif selected_agenda_item == "Exploring the dataset":
     # Filtering options
@@ -113,6 +116,83 @@ elif selected_agenda_item == "Exploring the dataset":
     # Display filtered data
     st.write("Displaying Results")
     st.write(filtered_data)
+
+elif selected_agenda_item == "Price Prediction":
+        # For reference :
+    #'one_hot_encoded_array' = encoding given column's with one hot encoder.
+    #'ohe.categories_' = list of category in each column as separate array.
+    #'categories' = coverting list of (names of) arrays as single array
+    #'encoded_dataframe' = converting encoded array and names of array as single dataframe
+    #'final' = concatenating encoded dataframe and original dataframe
+
+# Load the pickled models
+
+    ohe = s3.read_pickled_data_from_s3('ohe_pickle.pkl')
+    minmax_scaler = s3.read_pickled_data_from_s3('minmax_pickle.pkl')
+    forest = s3.read_pickled_data_from_s3('forest_pickle.pkl')
+
+    # Function to preprocess input data
+    def preprocess(raw_dataframe):
+        column = ['type_of_property', 'building_condition', 'kitchen_type', 'energy_class', 'heating_type']
+        one_hot_encoded_array = ohe.transform(raw_dataframe[column]).toarray()
+        categories = np.concatenate(ohe.categories_)
+        encoded_dataframe = pd.DataFrame(one_hot_encoded_array, columns=categories)
+        raw_dataframe = raw_dataframe.drop(columns=['type_of_property', 'building_condition', 'kitchen_type', 'energy_class', 'heating_type', 'postal_code'])
+        raw_dataframe = raw_dataframe.astype(int)
+        final = pd.concat([raw_dataframe, encoded_dataframe], axis=1)
+        final[['surface_of_the_plot', 'living_room_surface']] = minmax_scaler.transform(final[['surface_of_the_plot', 'living_room_surface']])
+        processed = final
+        return processed
+
+    # Function to make predictions
+    def predict_forest_price(processed):
+        predicted_price = forest.predict(processed)
+        return predicted_price[0]
+
+    # Streamlit app
+    def predict_house_or_apartment():
+        st.title("Property Price Prediction")
+
+        # Collect input values using Streamlit widgets
+        type_of_property = st.text_input("Type of Property:")
+        bedrooms = st.text_input("Bedrooms:")
+        energy_class = st.text_input("Energy Class:")
+        surface_of_the_plot = st.text_input("Surface of the Plot:")
+        living_room_surface = st.text_input("Living Room Surface:")
+        building_condition = st.text_input("Building Condition:")
+        bathrooms = st.text_input("Bathrooms:")
+        kitchen_type = st.text_input("Kitchen Type:")
+        heating_type = st.text_input("Heating Type:")
+        postal_code = st.text_input("Postal Code:")
+
+        # Button to submit the form and make predictions
+        if st.button("Predict"):
+            # Create a dictionary with input values
+            property_details = {
+                "type_of_property": type_of_property,
+                "bedrooms": bedrooms,
+                "energy_class": energy_class,
+                "surface_of_the_plot": surface_of_the_plot,
+                "living_room_surface": living_room_surface,
+                "number_of_frontages": 0,
+                "building_condition": building_condition,
+                "bathrooms": bathrooms,
+                "toilets": 0,
+                "kitchen_type": kitchen_type,
+                "heating_type": heating_type,
+                "postal_code": postal_code,
+            }
+
+            # Convert the dictionary to a DataFrame and preprocess
+            df = pd.DataFrame(property_details, index=[0])
+            processed = preprocess(df)
+
+            # Make predictions and display the result
+            predicted_price = predict_forest_price(processed)
+            st.success(f'Predicted Price: {predicted_price}')
+            return predicted_price
+        predict_house_or_apartment()
+
 
 elif selected_agenda_item == "Final notes":
     st.write("Thank you for exploring the dataset and visualisation.")
